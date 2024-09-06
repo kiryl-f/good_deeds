021d6ecae805bec1036c9e937f85ac2a8006cc02
Kiryl Filipovich <fkiryl@gmail.com>
2024-09-06 19:19:00 +0300

    fixing backend errors

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -9,5 +9,11 @@ export class User {
     name: string;
 
     @Column()
+    username: string;
+
+    @Column()
     email: string;
+
+    @Column()
+    password: string;
 }
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -19,7 +19,7 @@ export default function Register() {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
             Create a new account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600 ">
+          <p className="mt-2 text-center text-sm text-gray-600 mb-10">
             Or{' '}
             <Link href="/login" className="font-medium text-blue-600 hover:text-blue-500">
               login to your account
